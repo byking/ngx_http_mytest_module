c346b706f4c58a2a8870a95c6229fa66f089c3d6
byking <smithbyking@gmail.com>
2016-11-01 23:00:26 +0800

    delete extra newline

--- ngx_http_mytest_module.c
+++ ngx_http_mytest_module.c
@@ -20,9 +20,7 @@ static char *
 ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     ngx_http_core_loc_conf_t *clcf;
-    
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    
     clcf->handler = ngx_http_mytest_handler;
 
     return NGX_CONF_OK;
